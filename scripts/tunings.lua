446a26983a4e4b737b45556ca59c39d37f4dc10c
AsuraDiso <64610723+AsuraDiso@users.noreply.github.com>
2022-06-14 15:30:54 +0300

    Reworked tunnings

--- scripts/tunings.lua
+++ scripts/tunings.lua
@@ -10,25 +10,32 @@ local day_time = seg_time * day_segs
 local dusk_time = seg_time * dusk_segs
 local night_time = seg_time * night_segs
 
-TUNING.MOSQUITOSWARM_GROWTH = {
-	OFFSPRING_TIME = total_day_time * 5,
-	DESOLATION_RESPAWN_TIME = total_day_time * 50,
-	DEAD_DECAY_TIME = total_day_time * 30,
+
+local REFRESH_TUNING = {
+	MOSQUITOSWARM_GROWTH = {
+		OFFSPRING_TIME = total_day_time * 5,
+		DESOLATION_RESPAWN_TIME = total_day_time * 50,
+		DEAD_DECAY_TIME = total_day_time * 30,
+	},
+
+	MOSQUITOSWARM_COCOON_GROW_TIME =
+	{
+		{base=1.5*day_time, random=0.5*day_time},   --short
+		{base=5*day_time, random=2*day_time},   --normal
+		{base=5*day_time, random=2*day_time},   --tall
+		{base=1*day_time, random=0.5*day_time}   --old
+	},
+
+	MOSQUITOSWARM_RESTOCK = 15,
+
+	FUMEAGATOR_TARGETRANGE = 12,
+	FUMEAGATOR_DAMAGE = 50,
+	FUMEAGATOR_HEALTH = 1200,
+	FUMEAGATOR_ATTACKRANGE = 3.5,
+	FUMEAGATOR_ATTACKPERIOD = 2.5,
+	FUMEAGATOR_FUMEPERIOD = 20,
 }
 
-TUNING.MOSQUITOSWARM_COCOON_GROW_TIME =
-{
-	{base=1.5*day_time, random=0.5*day_time},   --short
-	{base=5*day_time, random=2*day_time},   --normal
-	{base=5*day_time, random=2*day_time},   --tall
-	{base=1*day_time, random=0.5*day_time}   --old
-}
-
-TUNING.MOSQUITOSWARM_RESTOCK = 15
-
-TUNING.FUMEAGATOR_TARGETRANGE = 12
-TUNING.FUMEAGATOR_DAMAGE = 50
-TUNING.FUMEAGATOR_HEALTH = 1200
-TUNING.FUMEAGATOR_ATTACKRANGE = 3.5
-TUNING.FUMEAGATOR_ATTACKPERIOD = 2.5
-TUNING.FUMEAGATOR_FUMEPERIOD = 20
+for key, value in pairs(REFRESH_TUNING) do
+    TUNING[key] = value
+end
