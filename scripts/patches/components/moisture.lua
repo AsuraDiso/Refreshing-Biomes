cbee5d278200af08c5c0f06110a41182d2f44425
Kipper <discordcccp@gmail.com>
2022-11-11 18:53:33 +0700

    случайно залил в гит игнор)

---
+++ scripts/patches/components/moisture.lua
@@ -0,0 +1,28 @@
+local easing = require("easing")
+
+return function(self)
+	local _GetMoistureRate = self.GetMoistureRate
+	function self:GetMoistureRate()
+        if self.inst._waketask then
+            local waterproofmult =
+            (   self.inst.components.sheltered ~= nil and
+                self.inst.components.sheltered.sheltered and
+                self.inst.components.sheltered.waterproofness or 0
+            ) +
+            (   self.inst.components.inventory ~= nil and
+                self.inst.components.inventory:GetWaterproofness() or 0
+            ) +
+            (   self.inherentWaterproofness or 0
+            ) +
+            (
+                self.waterproofnessmodifiers:Get() or 0
+            )
+            if waterproofmult >= 1 then
+                return 0
+            end
+            local rate = easing.inSine(TheWorld.state.precipitationrate, self.minMoistureRate, self.maxMoistureRate, 1)
+            return rate * (1 - waterproofmult)
+        end
+		return _GetMoistureRate(self)
+	end
+end
